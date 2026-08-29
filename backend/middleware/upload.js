c8df69f2870b0d549a071bdaa5e8fc3551b5c10d
Shivam Sharma <shivamsharmannl109@gmail.com>
2026-08-30 03:13:36 +0530

    refining code

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -1,17 +1,17 @@
 const multer = require("multer");
 const path = require("path");
 
-// Storage location and filename config
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "uploads/"); // Make sure this folder exists
+    cb(null, "uploads/"); 
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + path.extname(file.originalname));
   },
 });
 
-// File filter function to allow only images
+
 const fileFilter = (req, file, cb) => {
   const allowedTypes = /jpeg|jpg|png/;
 
@@ -27,7 +27,6 @@ const fileFilter = (req, file, cb) => {
   }
 };
 
-// Multer instance
 const upload = multer({ storage, fileFilter });
 
 module.exports = upload;
